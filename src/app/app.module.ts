b8067f7fbfe75e0246df28fc1f48069c72e3e488
Setenta30 <gonzalo_britez@hotmail.com>
2024-03-22 20:38:01 -0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageLayoutComponent } from './components/page-layout/page-layout.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageLayoutComponent,
+    ToolbarComponent,
+    SideMenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
